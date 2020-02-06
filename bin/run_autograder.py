6f309c7a811f01a6aa4098cbada02b7621c72b9b
poldrack <poldrack@gmail.com>
2020-02-06 11:38:30 -0800

    add interpreter

---
+++ bin/run_autograder.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import glob
+import argparse
+from pymongo.errors import ServerSelectionTimeoutError
+from pymongo import MongoClient
+
+from rautograder.Submission import Submission
+from rautograder.reports import make_report
+from rautograder.autograder import get_args, get_submission_files, process_submission
+
+
+if __name__ == '__main__':
+
+    # start mongodb (or at least make sure it's running)
+    try:
+        client = MongoClient(serverSelectionTimeoutMS=10)
+        # returns exception if mongo is not active
+        client.server_info() 
+    except ServerSelectionTimeoutError:
+        print('MongoDB is not running')
+        sys.exit(0)
+
+    args = get_args()
+
+    # load complete Pset Rmd 
+    master_submission = Submission(args.master_file, args.week)
+    # run and save variable values of interest to RData file for grading
+    master_submission.knit_rmd_file()
+    # source master, save file to master_Rdata
+    master_submission.source_r_file(rdata_path='master_Rdata')
+
+    # get list of student Rmd files from specified submissions directory
+    submission_files = get_submission_files(args.submission_dir)
+    submission_files = ['../data/submissions/dfVarError.Rmd']
+    # for each submission:
+    for submission_file in submission_files:
+
+        submission = process_submission(submission_file, master_submission,args.week)
+        make_report(submission)
+
+
+    # generate report for each student
+
+    # generate overall summary of grades
+
+
